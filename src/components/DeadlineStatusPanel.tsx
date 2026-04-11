508573452ed609ec4374d799c9bf9111be5150fd
goll <joaog.bastos@protonmail.ch>
2026-04-11 15:45:57 -0300

    Initial implementation of a deadline status panel

---
+++ src/components/DeadlineStatusPanel.tsx
@@ -0,0 +1,52 @@
+import { Card, ListGroup } from "solid-bootstrap";
+import type { Component, ParentComponent } from "solid-js";
+
+const WithDate: ParentComponent<{ date: Date }> = (props) => {
+    return (
+        <>
+            {/* JavaScript indexa meses a partir do zero */}
+            <b>
+                {props.date.getUTCDate().toString().padStart(2, "0")}/
+                {(props.date.getUTCMonth() + 1).toString().padStart(2, "0")}
+            </b>
+            <span class="mx-3 border-start border-dark-subtle"></span>
+            {props.children}
+        </>
+    );
+};
+
+const DeadlineStatusPanel: Component = () => {
+    return (
+        <Card class="w-75">
+            <Card.Header class="d-flex flex-row justify-content-between">
+                <span class="fw-bold">Deadlines</span>
+                {/* XXX: tornar isso um link */}
+                <span>Ver mais...</span>
+            </Card.Header>
+            <Card.Body>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>
+                        <WithDate date={new Date("2026-04-30")}>
+                            <i class="bi bi-box"></i>
+                            <span class="mx-3">Entrega do pedido Foo</span>
+                        </WithDate>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <WithDate date={new Date("2026-05-01")}>
+                            <i class="bi bi-box"></i>
+                            <span class="mx-3">Entrega do pedido Bar</span>
+                        </WithDate>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <WithDate date={new Date("2026-05-05")}>
+                            <i class="bi bi-currency-dollar"></i>
+                            <span class="mx-3">Pagamento de Baz</span>
+                        </WithDate>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default DeadlineStatusPanel;
