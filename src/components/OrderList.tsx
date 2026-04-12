cecf5c9a61bab885198a0cd976ba1b69b3e48e73
goll <joaog.bastos@protonmail.ch>
2026-04-12 19:52:22 -0300

    "Finish" the OrderList component

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,4 +1,4 @@
-import { For, type Component, type JSX } from "solid-js";
+import { For, Index, Show, type Component } from "solid-js";
 
 import style from "./styles/OrderList.module.scss";
 
@@ -18,16 +18,44 @@ type Order = {
     description: string;
     amountsPerSize: number[];
     amountsPerSizeFinished: number[];
+    expectedEndDate?: Date;
+    actualEndDate?: Date;
     status: "paid" | "delivered" | "finished" | "pending" | "not-started";
 };
 
 const DUMMY_ORDERS: Order[] = [
     {
-        supplier: "Fornecedor Foo",
+        supplier: "Foo",
         description: "Pedido XYZ",
         amountsPerSize: [300, 150, 225, 120],
         amountsPerSizeFinished: [300, 150, 225, 120],
         status: "paid",
+        expectedEndDate: new Date("2026-04-11"),
+        actualEndDate: new Date("2026-04-14"),
+    },
+    {
+        supplier: "Foo",
+        description: "Pedido ABC",
+        amountsPerSize: [100, 38, 0, 44],
+        amountsPerSizeFinished: [100, 38, 0, 44],
+        status: "finished",
+        expectedEndDate: new Date("2026-04-12"),
+        actualEndDate: new Date("2026-04-12"),
+    },
+    {
+        supplier: "Quux",
+        description: "Pedido UVW",
+        amountsPerSize: [200, 0, 0, 0],
+        amountsPerSizeFinished: [30, 0, 0, 0],
+        status: "pending",
+        expectedEndDate: new Date("2026-04-28"),
+    },
+    {
+        supplier: "Nome muito grande",
+        description: "Pedido MNP",
+        amountsPerSize: [300, 500, 0, 0],
+        amountsPerSizeFinished: [0, 0, 0, 0],
+        status: "not-started",
     },
 ];
 
@@ -37,15 +65,17 @@ const OrderItem: Component<{ id: string; order: Order }> = (props) => {
         (prev, curr) => prev + curr,
     );
 
-    const possibleBadges: { [K in Order["status"]]: () => JSX.Element } = {
-        paid: () => <Badge bg="primary"></Badge>,
-        delivered: () => <Badge bg="success">Entregue</Badge>,
-        finished: () => <Badge bg="finished">Pronto</Badge>,
-        pending: () => <Badge bg="warning">Em produção</Badge>,
-        "not-started": () => <Badge bg="danger">Não iniciado</Badge>,
+    const badgeMap: { [K in Order["status"]]: [string, string] } = {
+        paid: ["primary", "Pago"],
+        delivered: ["success", "Entregue"],
+        finished: ["success", "Pronto"],
+        pending: ["warning", "Em produção"],
+        "not-started": ["danger", "Não iniciado"],
     };
 
-    const badge = possibleBadges[props.order.status];
+    const amountMap = ["P", "M", "G", "GG"];
+
+    const [badgeStyle, badgeMessage] = badgeMap[props.order.status];
 
     return (
         <Accordion.Item eventKey={props.id}>
@@ -54,7 +84,7 @@ const OrderItem: Component<{ id: string; order: Order }> = (props) => {
                     <i class={`bi bi-box ${style["icon-bold"]}`}></i>
                     {props.order.description}
                 </div>
-                {badge()}
+                <Badge bg={badgeStyle}>{badgeMessage}</Badge>
                 <Button
                     variant="body"
                     class="mx-3"
@@ -63,7 +93,82 @@ const OrderItem: Component<{ id: string; order: Order }> = (props) => {
                     <i class="bi bi-pencil-square text-body-secondary align-top"></i>
                 </Button>
             </Accordion.Header>
-            <Accordion.Body></Accordion.Body>
+            <Accordion.Body>
+                <div class="d-flex flex-row gap-5">
+                    <div>
+                        <span class="fw-bold">Fornecedor</span>
+                        <p>{props.order.supplier}</p>
+                    </div>
+                    <div>
+                        <span class="fw-bold">Quantidades</span>
+                        <div class="d-flex flex-row gap-4">
+                            <Index each={amountMap}>
+                                {(tag, index) => {
+                                    const partial =
+                                        props.order.amountsPerSizeFinished[
+                                            index
+                                        ];
+
+                                    const total =
+                                        props.order.amountsPerSize[index];
+
+                                    if (total === 0) {
+                                        return <></>;
+                                    }
+
+                                    return (
+                                        <div>
+                                            <div>{tag()}</div>
+                                            <p>
+                                                {partial}/{total}
+                                            </p>
+                                        </div>
+                                    );
+                                }}
+                            </Index>
+                        </div>
+                    </div>
+                    {/* XXX: this is horrible */}
+                    <Show when={props.order.expectedEndDate}>
+                        {(date) => (
+                            <div>
+                                <span class="fw-bold">
+                                    Data de Término Prevista
+                                </span>
+                                <p>
+                                    {date()
+                                        .getUTCDate()
+                                        .toString()
+                                        .padStart(2, "0")}
+                                    /
+                                    {(date().getUTCMonth() + 1)
+                                        .toString()
+                                        .padStart(2, "0")}
+                                    /{date().getUTCFullYear().toString()}
+                                </p>
+                            </div>
+                        )}
+                    </Show>
+                    <Show when={props.order.actualEndDate}>
+                        {(date) => (
+                            <div>
+                                <span class="fw-bold">Data de Término</span>
+                                <p>
+                                    {date()
+                                        .getUTCDate()
+                                        .toString()
+                                        .padStart(2, "0")}
+                                    /
+                                    {(date().getUTCMonth() + 1)
+                                        .toString()
+                                        .padStart(2, "0")}
+                                    /{date().getUTCFullYear().toString()}
+                                </p>
+                            </div>
+                        )}
+                    </Show>
+                </div>
+            </Accordion.Body>
         </Accordion.Item>
     );
 };
@@ -108,7 +213,7 @@ const OrderList: Component = () => {
             <Accordion class="py-4">
                 <For each={DUMMY_ORDERS}>
                     {(order, index) => (
-                        <OrderItem id={`${index}`} order={order} />
+                        <OrderItem id={`${index()}`} order={order} />
                     )}
                 </For>
             </Accordion>
