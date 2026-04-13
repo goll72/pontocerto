8db67c1fe8e7abc2282e45f01b0067855b4bd7e0
goll <joaog.bastos@protonmail.ch>
2026-04-13 14:09:52 -0300

    Change SCSS file structure so the codebase can integrate with Bootstrap's SCSS in a more seamless way

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,26 +5,28 @@ import Logo from "/assets/logo.svg?component-solid";
 
 const Sidebar: Component = () => {
     return (
-        <div class={`d-flex flex-md-column flex-row p-2 p-md-3 bg-primary ${style.color} text-white align-items-center py-2 py-md-3`}>
+        <div
+            class={`position-fixed d-flex flex-md-column flex-row p-2 p-md-3 bg-primary text-white align-items-center py-2 py-md-3 ${style["sticky-sidebar"]}`}
+        >
             <div class="d-flex flex-row mb-1 px-2 gap-3">
                 <a
                     class="nav-link icon-link"
                     aria-current="page"
-                    href="/home/" onclick={(e) => {
-                        if(window.location.pathname === "/home/"){
+                    href="/home/"
+                    onclick={(e) => {
+                        if (window.location.pathname === "/home/") {
                             e.preventDefault();
                         }
-                    }} 
+                    }}
                 >
-                    <Logo class={`${style.logo} image-fluid`}/>
+                    <Logo class={`${style.logo} image-fluid`} />
                 </a>
 
                 <h1 class="fw-bold font-monospace d-none d-md-inline">
-                        PONTO
-                        <br />
-                        CERTO
-                    </h1>
-                
+                    PONTO
+                    <br />
+                    CERTO
+                </h1>
             </div>
 
             <ul class="nav nav-pills flex-md-column flex-row justify-content-md-start justify-content-center gap-2">
@@ -33,31 +35,17 @@ const Sidebar: Component = () => {
                         class="nav-link icon-link icon-link-hover text-white gap-1"
                         style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
                         aria-current="page"
-                        href="/orders/" onclick={(e) => {
-                            if(window.location.pathname === "/orders/"){
+                        href="/orders/"
+                        onclick={(e) => {
+                            if (window.location.pathname === "/orders/") {
                                 e.preventDefault();
                             }
                         }}
                     >
-                        <i class={`bi bi-box ${style.icon}`}></i>
-                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">Pedido</span>
-                    </a>
-
-                </li>
-
-                <li class="nav-item">
-                    <a
-                        class="nav-link icon-link icon-link-hover text-white gap-1"
-                        style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
-                        aria-current="page"
-                        href="/finances/" onclick={(e) => {
-                            if(window.location.pathname === "/finances/"){
-                                e.preventDefault();
-                            }
-                        }}
-                    >
-                        <i class={`bi bi-wallet ${style.icon}`}></i>
-                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">Caixa</span>
+                        <i class={`bi bi-box ${style["bold-icon"]}`}></i>
+                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">
+                            Pedido
+                        </span>
                     </a>
                 </li>
 
@@ -66,14 +54,17 @@ const Sidebar: Component = () => {
                         class="nav-link icon-link icon-link-hover text-white gap-1"
                         style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
                         aria-current="page"
-                        href="/documents/" onclick={(e) => {
-                            if(window.location.pathname === "/documents/"){
+                        href="/finances/"
+                        onclick={(e) => {
+                            if (window.location.pathname === "/finances/") {
                                 e.preventDefault();
                             }
                         }}
                     >
-                        <i class={`bi bi-folder ${style.icon}`}></i>
-                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">Documentos</span>
+                        <i class={`bi bi-wallet ${style["bold-icon"]}`}></i>
+                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">
+                            Caixa
+                        </span>
                     </a>
                 </li>
 
@@ -81,14 +72,18 @@ const Sidebar: Component = () => {
                     <a
                         class="nav-link icon-link icon-link-hover text-white gap-1"
                         style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
-                        href="/suppliers/" onclick={(e) => {
-                            if(window.location.pathname === "/suppliers/"){
+                        aria-current="page"
+                        href="/documents/"
+                        onclick={(e) => {
+                            if (window.location.pathname === "/documents/") {
                                 e.preventDefault();
                             }
                         }}
                     >
-                        <i class={`bi bi-buildings ${style.icon}`}></i>
-                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">Fornecedores</span>
+                        <i class={`bi bi-folder ${style["bold-icon"]}`}></i>
+                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">
+                            Documentos
+                        </span>
                     </a>
                 </li>
 
@@ -96,18 +91,40 @@ const Sidebar: Component = () => {
                     <a
                         class="nav-link icon-link icon-link-hover text-white gap-1"
                         style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
-                        href="/home/" onclick={(e) => {
-                            if(window.location.pathname === "/home/"){
+                        href="/suppliers/"
+                        onclick={(e) => {
+                            if (window.location.pathname === "/suppliers/") {
                                 e.preventDefault();
                             }
                         }}
                     >
-                        <i class={`bi bi-box-arrow-left ${style.icon}`}></i>
-                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">Sair</span>
+                        <i class={`bi bi-buildings ${style["bold-icon"]}`}></i>
+                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">
+                            Fornecedores
+                        </span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a
+                        class="nav-link icon-link icon-link-hover text-white gap-1"
+                        style="--bs-icon-link-transform: translate3d(0, -.51rem, 0);"
+                        href="/home/"
+                        onclick={(e) => {
+                            if (window.location.pathname === "/home/") {
+                                e.preventDefault();
+                            }
+                        }}
+                    >
+                        <i
+                            class={`bi bi-box-arrow-left ${style["bold-icon"]}`}
+                        ></i>
+                        <span class="mx-3 fw-bold fs-5 d-none d-md-inline">
+                            Sair
+                        </span>
                     </a>
                 </li>
             </ul>
-
         </div>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,22 @@ import solidSvg from "vite-plugin-solid-svg";
 import devtools from "solid-devtools/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+const page = (endpoint: string) =>
+    `${import.meta.dirname}/src${endpoint}index.html`;
+
 export default {
+    appType: "mpa",
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                silenceDeprecations: [
+                    // Não temos como escapar do `@import` ao usar Bootstrap
+                    "import",
+                ],
+            },
+        },
+    },
     plugins: [devtools(), solidPlugin(), solidSvg(), VitePWA()],
     server: {
         port: 3000,
@@ -13,6 +28,14 @@ export default {
     build: {
         target: "esnext",
         outDir: "../dist",
+        emptyOutDir: true,
+        rolldownOptions: {
+            input: {
+                main: page("/"),
+                home: page("/home/"),
+                orders: page("/orders/"),
+            },
+        },
     },
     root: "src",
     resolve: {
