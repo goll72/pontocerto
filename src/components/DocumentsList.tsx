585a78da57dfd53f49f4499c499a94d6ec8dd242
Marcelo-MConti <contimarcelao@gmail.com>
2026-04-13 14:55:30 -0300

    File list card

--- src/components/DocumentsList.tsx
+++ src/components/DocumentsList.tsx
@@ -14,6 +14,41 @@ type Folder = {
     items: number;
 };
 
+type File = {
+    name: string;
+    size: string;
+}
+
+
+const DUMMYFiles = [
+    { name: "relatrorio.pdf", size:"2.4 MB"},
+    { name: "contrato.docx", size: "1.1 MB"},
+    { name: "Planilha.xlsx", size: "3.0 MB"},
+    { name: "ModeloJeans.png", size: "3.0 MB"},
+];
+
+const Filecard: Component<{ file: File }> = (props) => {
+    return (
+        <button
+            type="button"
+            class="p-3 border rounded d-flex align-items-center gap-3 w-100 bg-white"
+            style="cursor: pointer"
+        >
+
+            <i class="bi bi-file-earmark fs-3 text-primary"></i>
+
+            <div class="flex-grow-1 text-start">
+                    <div class="fw-bold text-truncate">
+                        {props.file.name}
+                    </div>
+                    <div class="text-muted">
+                        {props.file.size}
+                    </div>
+            </div>
+        </button> 
+    );
+};
+
 const DUMMYFolders: Folder[] = [
     { name: "Contratos de Funcionarios", items: 20 },
     { name: "Exames admissionais/demissional", items: 30 },
@@ -90,6 +125,33 @@ const FolderSection: Component = () => {
                         </Col>
                     )}
                 </For>
+            </Row>
+
+            <Row class="py-3">
+                <Col xs={12} sm={3} md={4} lg={9}>
+                    <span class="h5 fw-bold">Arquivos</span>
+                </Col>
+
+                <Col
+                    xs={{ span: 4, offset: 8 }}
+                    md={{ span: 6, offset: 6 }}
+                    lg={{ span: 3, offset: 0 }}
+                    class="d-flex flex-row-reverse"
+                >
+                    <Button variant="primary">
+                        <i class="bi bi-upload"></i> Upload
+                    </Button>
+                </Col>
+            </Row>
+
+            <Row class="g-2">
+                    <For each={DUMMYFiles}>
+                        {(file)=>(
+                            <Col xs={12} md={6} lg={4}>
+                                <Filecard file={file} />
+                            </Col>
+                        )}
+                    </For>
             </Row>
         </Container>
     );
